a251eef1ad0637943c731c60813046db6db5ca11
Your Name <you@example.com>
2026-09-16 13:33:11 -0600

    h2 para test unitarios

--- src/test/java/com/twitchspamdetector/coreapi/CoreApiApplicationTests.java
+++ src/test/java/com/twitchspamdetector/coreapi/CoreApiApplicationTests.java
@@ -2,11 +2,11 @@ package com.twitchspamdetector.coreapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CoreApiApplicationTests {
-
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 }
